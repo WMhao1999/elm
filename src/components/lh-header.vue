78a8fdfc649897e40d5ee052ebbf37fddb44c843
e <2038497634@qq.com>
2019-11-10 18:51:36 +0800

    sy

--- src/components/lh-header.vue
+++ src/components/lh-header.vue
@@ -1,91 +1,93 @@
 <template>
-	<div id="l_header">
-		<div class="l_bd">
-			<div class="l_l">
-				<slot name="left"><span class="iconfont" style="color: #FFFFFF;" @click="$router.go(-1)">&#xe642;</span></slot>
-			</div>
-			<div class="l_c">{{tit_a}}</div>
-			<div class="l_r">
-				<slot name="right"></slot>
-			</div>
-		</div>
-		<div style="min-height:1.38rem"></div>
-	</div>
+  <div id="l_header">
+    <div class="l_bd">
+      <div class="l_l">
+        <slot name="left">
+          <span class="iconfont" style="color: #FFFFFF;" @click="$router.go(-1)">&#xe642;</span>
+        </slot>
+      </div>
+      <div class="l_c">{{tit_a}}</div>
+      <div class="l_r">
+        <slot name="right"></slot>
+      </div>
+    </div>
+    <div style="min-height:1.68rem"></div>
+  </div>
 </template>
 
 <script>
-	export default {
-		props: ["title"],
-		data() {
-			return {
-				tit: "",
-			};
-		},
-		computed: {
-			tit_a: {
-				get() {
-					return (this.tit = this.title);
-				},
-				set(v) {}
-			},
-			l_pr_a: {
-				get() {
-					return (this.l_pr = this.path_r);
-				}
-			}
-		}
-	};
+export default {
+  props: ["title"],
+  data() {
+    return {
+      tit: ""
+    };
+  },
+  computed: {
+    tit_a: {
+      get() {
+        return (this.tit = this.title);
+      },
+      set(v) {}
+    },
+    l_pr_a: {
+      get() {
+        return (this.l_pr = this.path_r);
+      }
+    }
+  }
+};
 </script>
 
 <style>
-	#l_header {
-		font-size: 0.52rem;
-	}
+#l_header {
+  font-size: 0.52rem;
+}
 
-	#l_header a div {
-		color: #FFFFFF;
-		font-size: 0.52rem;
-	}
+#l_header a div {
+  color: #ffffff;
+  font-size: 0.52rem;
+}
 
-	#l_header a div span {
-		color: #FFFFFF;
-		font-size: 0.35rem;
-	}
+#l_header a div span {
+  color: #ffffff;
+  font-size: 0.35rem;
+}
 
-	#l_header .l_bd {
-		display: flex;
-		width: 100%;
-		padding: 0.34rem;
-		box-sizing: border-box;
-		background: #358fe7;
-		position: fixed;
-		top: 0;
-		overflow: hidden;
-	}
+#l_header .l_bd {
+  display: flex;
+  width: 100%;
+  padding: 0.34rem;
+  box-sizing: border-box;
+  background: #358fe7;
+  position: fixed;
+  top: 0;
+  overflow: hidden;
+}
 
-	#l_header .l_l {
-		padding-right: 0.4rem;
-		float: left;
-		width: 20%;
-	}
+#l_header .l_l {
+  padding-right: 0.4rem;
+  float: left;
+  width: 20%;
+}
 
-	#l_header .l_c {
-		font-size: 0.52rem;
-		text-align: center;
-		flex: 1;
-		color: #fff;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-		width: 60%;
-	}
+#l_header .l_c {
+  font-size: 0.52rem;
+  text-align: center;
+  flex: 1;
+  color: #fff;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 60%;
+}
 
-	#l_header .l_r {
-		width: 20%;
-		padding-left: 0.4rem;
-		float: right;
-	}
-	#l_header .l_r a div{
-		font-size: 0.45rem;
-	}
+#l_header .l_r {
+  width: 20%;
+  padding-left: 0.4rem;
+  float: right;
+}
+#l_header .l_r a div {
+  font-size: 0.45rem;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import resource from 'vue-resource'
+// import resource from 'vue-resource'
 import '../font/iconfont.css'
 import '../font/iconfont.js'
-Vue.use(resource)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
+// Vue.use(resource)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-
+// Vue.http.options.xhr = { withCredentials: true }
+axios.defaults.withCredentials = true
 new Vue({
   router,
   store,
