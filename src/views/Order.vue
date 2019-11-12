6861aed2df4193e06c8fd892dc98fbc9cf876898
Halohk2251 <1354010731@qq.com>
2019-11-12 10:14:19 +0800

    10：14 whk

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,36 +1,43 @@
 <template>
-	<div id="order">
-		<transition name="fade">
-			<router-view />
-		</transition>
-		<div class="foot">
-			<div>
-				<router-link to="/order/">
-					<div class="iconfont icon-changyonglogo40
-"></div>
-					<p>外卖</p>
-				</router-link>
-			</div>
-			<div>
-				<router-link to="/order/search">
-					<div class="iconfont icon-Safari"></div>
-					<p>搜索</p>
-				</router-link>
-			</div>
-			<div>
-				<router-link to="/order/form">
-					<div class="iconfont icon-weibiaoti-"></div>
-					<p>订单</p>
-				</router-link>
-			</div>
-			<div>
-				<router-link to="/order/myself">
-					<div class="iconfont icon-wode"></div>
-					<p>我的</p>
-				</router-link>
-			</div>
-		</div>
-	</div>
+  <div id="order">
+    <transition name="fade">
+      <router-view />
+    </transition>
+    <div class="foot">
+      <div>
+        <router-link to="/order/">
+          <svg class="icon" aria-hidden="true" style="font-size:0.8rem;width:0.8rem;height:0.8rem">
+            <use xlink:href="#icon-waimai1" />
+          </svg>
+          <p class="wenzi">外卖</p>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/order/search">
+                  <svg class="icon" aria-hidden="true" style="font-size:0.7rem;width:0.7rem;height:0.7rem">
+            <use xlink:href="#icon-sousuo" />
+          </svg>
+          <p class="www">搜索</p>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/order/form">
+          <svg class="icon" aria-hidden="true" style="font-size:0.8rem;width:0.8rem;height:0.8rem">
+            <use xlink:href="#icon-icon-" />
+          </svg>
+          <p class="wenzi">订单</p>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/order/myself">
+          <svg class="icon" aria-hidden="true" style="font-size:0.8rem;width:0.8rem;height:0.8rem">
+            <use xlink:href="#icon-wodedangxuan1" />
+          </svg>
+          <p class="wenzi">我的</p>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -44,7 +51,12 @@ body {
   background: red;
   position: relative;
 }
-
+.wenzi {
+  padding-top: 0.2rem;
+}
+.www{
+	 padding-top: 0.25rem;
+}
 .foot {
   position: fixed;
   bottom: 0;
@@ -60,31 +72,31 @@ body {
   text-align: center;
 }
 
-	.foot>div div {
-		width: 0.8rem;
-		height: 0.8rem;
-		/* background: yellow; */
-		display: inline-block;
-		margin-bottom: 0.2rem;
-			line-height: 0.8rem;
-	}
-	.icon-changyonglogo40{
-		width: 0.8rem;
-		font-size: 0.8rem
-	}
-	.icon-Safari{
-			font-size: 0.5rem;
-		width: 0.8rem;
-		height: 0.8rem;
-	}
-	.icon-weibiaoti-{
-			font-size: 0.6rem;
-		width: 0.8rem;
-		height: 0.8rem;
-	}
-	.icon-wode{
-			font-size: 0.55rem;
-		width: 0.8rem;
-		height: 0.8rem;
-	}
+.foot > div div {
+  width: 0.8rem;
+  height: 0.8rem;
+  /* background: yellow; */
+  display: inline-block;
+  margin-bottom: 0.2rem;
+  /* line-height: 0.9rem; */
+}
+.icon-changyonglogo40 {
+  width: 0.8rem;
+  font-size: 0.8rem;
+}
+.icon-Safari {
+  font-size: 0.5rem;
+  width: 0.8rem;
+  height: 0.8rem;
+}
+.icon-weibiaoti- {
+  font-size: 0.6rem;
+  width: 0.8rem;
+  height: 0.8rem;
+}
+.icon-wode {
+  font-size: 0.55rem;
+  width: 0.8rem;
+  height: 0.8rem;
+}
 </style>
---
+++ src/views/duihuan.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="duihuan">
+    <lh_header :title="'兑换红包'"></lh_header>
+    <div class="inp">
+      <input type="text" placeholder="请输入兑换码" />
+    </div>
+    <div class="inp_1">
+      <div class="inp_2">
+          <input type="text" placeholder="验证码"/>
+      </div>
+        <div class="inp_3">
+            <div class="yanz_img">
+                <img :src="imgurl" alt="">
+            </div>
+            <div class="inp_4">
+                <p>看不清</p>
+                <p @click="yanz" class="huan">换一张</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="btt">
+          <div class="btt_a">
+              <span>兑换</span>
+          </div>
+      </div>
+     
+  </div>
+</template>
+
+<script>
+import lh_header from "./../components/lh-header.vue";
+export default {
+  components: {
+    lh_header
+  },
+  data() {
+    return {
+      imgurl: ""
+    };
+  },
+  mounted() {
+    this.yanz();
+  },
+  methods: {
+    yanz() {
+      this.axios.post("https://elm.cangdu.org/v1/captchas").then(res => {
+        console.log(res.data.code);
+        this.imgurl = res.data.code;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.huan{
+    color:#5b90c6;
+}
+.yanz_img{
+    width: 60%;
+    float: left;
+}
+.inp_5{
+    width: 15%;
+}
+.inp {
+  text-align: center;
+  padding: 0.48rem 0.33rem;
+}
+.inp input {
+  width: 90%;
+  padding: 0.4rem;
+  border: none;
+}
+.inp_1{
+     padding: 0.28rem 0.5rem;
+    overflow: hidden;
+}
+.inp_2{
+    width: 60%;
+    float: left;
+}
+.inp_2 input{
+    padding: 0.5rem;
+  border: none;
+}
+.yanz_img{
+    float: left;
+}
+.inp_3{
+    background: #fff;
+    padding: 0.2rem 0rem;
+    width: 40%;
+    float: left;
+}
+.btt{
+     padding: 0.28rem 0.5rem;
+    overflow: hidden;
+}
+.btt_a{
+    background: #ccc;
+    border-radius: 5%;
+    padding: 0.38rem 0;
+    text-align: center;
+   
+}
+.btt_a span{
+ color: #fff;
+ font-size: 0.42rem
+}
+</style>
